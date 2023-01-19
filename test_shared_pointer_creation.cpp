33d9447fc9fe1a119ad1d07404abda65377f8dc2
hriday <hriday.bavle@gmail.com>
2023-01-19 14:42:25 +0100

    Apply clang format

--- test_shared_pointer_creation.cpp
+++ test_shared_pointer_creation.cpp
@@ -1,52 +1,35 @@
-#include <memory>
+#include <unistd.h>
+
 #include <iostream>
+#include <memory>
+#include <mutex>
 #include <thread>
 #include <vector>
-#include <mutex>
-#include <unistd.h>
 
 using namespace std;
 
-
-struct A
-{
-  std::vector<int> a;
-  A(std::vector<int> a) : a(a) {}
-
+struct A {
+  int a;
+  A(int a) : a(a) {}
 };
 
-std::mutex a_lock;
-
-void func_change_a(shared_ptr<A> a) {
-    shared_ptr<A> b = a;
-    int counter=0;
-    while(counter < 1000) {    
-        b->a[0] = counter+10;
-        cout << "b value inside func: " << b->a[0] << endl; 
-        counter++;
-    }       
-}
-
-void func_increse_a_size(shared_ptr<A> a) {
-    int counter=0;
-    while(counter < 1000) {
-        a->a[0] = counter+10;
-        cout << "a size inside func insrease a size: " << a->a[0] << endl; 
-        counter++;
-    }
-}
-
 int main(int argc, char **argv) {
-    std::cout << "Inside Main" << std::endl;
-    std::vector<int> num; num.push_back(1);
-    shared_ptr<A> a(new A(num));
-    a->a.resize(1000);
+  std::cout << "Inside Main" << std::endl;
 
-    std::thread t1(func_change_a, a);
-    std::thread t2(func_increse_a_size, a);
+  int num = 1;
+  shared_ptr<A> obj1;
+  shared_ptr<A> obj2;
 
-    t1.join();   
-    t2.join();
-                                                         
-    return 0;
+  obj1 = make_shared<A>(num);
+  std::cout << "obj1 a value: " << obj1->a << std::endl;
+  std::cout << "obj1 use count: " << obj1.use_count() << std::endl;
+
+  num = 2;
+  obj2 = obj1;
+  std::cout << "obj1 a value: " << obj1->a << std::endl;
+  std::cout << "obj1 use count: " << obj1.use_count() << std::endl;
+
+  // std::cout << "obj2 a value: " << obj2->a << std::endl;
+
+  return 0;
 }
